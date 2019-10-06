dfde93166ba00a28b903795c416e3cd484eab905
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-10-06 11:09:24 -0400

    add demo-touch basic function

---
+++ Demo#2/Assets/Scripts/ReturnPositionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnPositionController : MonoBehaviour {
+
+    public bool isReturned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Controller")) {
+            if (GameManager.instance.targetBlock.isTouched) {
+                isReturned = true;
+            }
+        }
+    }
+}
