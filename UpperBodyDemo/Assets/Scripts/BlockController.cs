285d18aaf3df6af55d19fc70f7db39d455bfb375
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-10-10 14:32:42 -0400

    add new demo to replace the old one

---
+++ UpperBodyDemo/Assets/Scripts/BlockController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+    public MeshRenderer blockRenderer;
+
+    public bool isTarget;
+
+    public bool isTouched;
+
+    public Color targetColor;
+    public Color originalColor;
+    public Color targetedColor;
+    public Color touchedColor;
+    public float colorChangeSpeed;
+    // Start is called before the first frame update
+    void Start() {
+        originalColor = blockRenderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        blockRenderer.material.color = Color.Lerp(blockRenderer.material.color,targetColor,colorChangeSpeed);
+        if (isTarget) {
+            targetColor = targetedColor;
+        }
+
+        if (isTouched) {
+            targetColor = touchedColor;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("1s");
+        if (other.gameObject.CompareTag("Controller")) {
+            GameManager.instance.scores += 1;
+            isTouched = true;
+            isTarget = false;
+        }
+    }
+}
---
+++ UpperBodyDemo/Assets/Scripts/ReturnPositionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnPositionController : MonoBehaviour {
+
+    public bool isReturned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Controller")) {
+            if (GameManager.instance.targetBlock!=null && GameManager.instance.targetBlock.isTouched) {
+                GameManager.instance.targetBlock.isTouched = false;
+                GameManager.instance.targetBlock.targetColor = GameManager.instance.targetBlock.originalColor;
+                Debug.Log("Origin");
+                isReturned = true;
+            }
+        }
+    }
+}
